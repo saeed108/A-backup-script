5e50cd77cc154f4d9d718df72b37d90b790dfbf8
Saeed Sadeghi <saeed.sadeghi@ericsson.com>
2023-01-30 01:02:01 +0100

    mad frame

---
+++ A backup script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+   echo "Fel: 2 argument krävs - Mappen som ska säkerhetskopieras, remotas IP och destinationsmappen för säkerhetskopieringsarkivet."
+   exit 1
+fi
+
+#Source directory and target directory.
+src_dir=$1
+target_dir=$2
+
+if [[  ]]; then
+
+else
+
+
+fi
